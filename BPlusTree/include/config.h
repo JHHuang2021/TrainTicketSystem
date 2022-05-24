7fc2123034a34717c37cecef1c8043e9d9b3bc92
JHHuang2021 <huang_jh030609@163.com>
2022-05-24 21:45:43 +0800

    ~

--- BPlusTree/include/config.h
+++ BPlusTree/include/config.h
@@ -7,7 +7,7 @@ namespace huang {
 #define INVALID_PAGE_ID -1   // representing an invalid page id
 #define INVALID_TXN_ID -1    // representing an invalid txn id
 #define HEADER_PAGE_ID 0     // the header page id
-#define PAGE_SIZE 40000        // size of a data page in byte
+#define PAGE_SIZE 400000        // size of a data page in byte
 #define BUFFER_POOL_SIZE 10  // size of buffer pool
 
 typedef int page_id_t;  // page id type
