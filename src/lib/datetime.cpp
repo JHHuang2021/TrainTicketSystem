f96620c119fe4c55d9ff65e50674ae83165b92b6
1024th <lintianchuan@foxmail.com>
2022-06-10 00:47:37 +0800

    feat(datetime): improve performance

--- src/lib/datetime.cpp
+++ src/lib/datetime.cpp
@@ -3,25 +3,87 @@
 namespace lin {
 
 namespace {
-constexpr int kDaysPerMonth[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-constexpr int kSumDays[] = {0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365};
+constexpr const int kDaysPerMonth[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+constexpr const int kSumDays[] = {0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365};
 inline void SetNumber(char *s, int num) {
   s[0] = num / 10 + '0';
   s[1] = num % 10 + '0';
 }
+inline int GetNumber(const char *s) { return (s[0] - '0') * 10 + s[1] - '0'; }
 }  // namespace
 
-DateTime::DateTime(const std::string &str) {
+Time::Time(std::string_view str) {
+  // str:  hh:mm
+  // pos:  01234
+  int hour = GetNumber(str.data());
+  int min = GetNumber(str.data() + 3);
+  minutes_ = hour * 60 + min;
+}
+
+std::pair<int, int> Time::GetHourMinute() const {
+  int min = minutes_ % 60, hour = min / 60;
+  return std::make_pair(hour, min);
+}
+
+std::string Time::ToString() const {
+  auto [hour, min] = GetHourMinute();
+  // char buf[6];
+  std::string buf(5, '\0');
+  // str:  hh:mm
+  // pos:  01234
+  SetNumber(buf.data(), hour), buf[2] = ':', SetNumber(buf.data() + 3, min);
+  return buf;
+}
+Time Time::operator+(Duration o) const { return Time(minutes_ + o.minutes_); }
+Time &Time::operator+=(Duration o) {
+  this->minutes_ += o.minutes_;
+  return *this;
+}
+Duration Time::operator-(const Time &o) const { return Duration(minutes_ - o.minutes_); }
+
+Date::Date(std::string_view str) {
+  // str:  MM-dd
+  // pos:  01234
+  int month = GetNumber(str.data());
+  int day = GetNumber(str.data() + 3);
+  minutes_ = (kSumDays[month - 1] + day) * 24 * 60;
+}
+
+std::pair<int, int> Date::GetMonthDay() const {
+  int days = minutes_ / (60 * 24);
+  int month;
+  for (month = 1; month <= 12; ++month) {
+    if (days <= kSumDays[month]) {
+      days -= kSumDays[month - 1];
+      break;
+    }
+  }
+  return std::make_pair(month, days);
+}
+
+std::string Date::ToString() const {
+  auto [month, day] = GetMonthDay();
+  // char buf[6];
+  std::string buf(5, '\0');
+  // str:  MM-dd
+  // pos:  01234
+  SetNumber(buf.data(), month), buf[2] = '-', SetNumber(buf.data() + 3, day);
+  return buf;
+}
+DateTime Date::operator+(Time o) const { return DateTime(minutes_ + o.minutes_); }
+
+
+DateTime::DateTime(std::string_view str) {
   // str:  MM-dd hh:mm
   // pos:  0123456789
-  int month = std::stoi(str.substr(0, 2));
-  int day = std::stoi(str.substr(3, 2));
-  int hour = std::stoi(str.substr(6, 2));
-  int min = std::stoi(str.substr(9, 2));
+  int month = GetNumber(str.data());
+  int day = GetNumber(str.data() + 3);
+  int hour = GetNumber(str.data() + 6);
+  int min = GetNumber(str.data() + 9);
   minutes_ = ((kSumDays[month - 1] + day) * 24 + hour) * 60 + min;
 }
 
-std::pair<int, int> DateTime::GetDate() const {
+std::pair<int, int> DateTime::GetMonthDay() const {
   int days = minutes_ / (60 * 24);
   // int month = std::lower_bound(kSumDays + 1, kSumDays + 13, days) - kSumDays;
   // days -= kSumDays[month - 1];
@@ -36,22 +98,24 @@ std::pair<int, int> DateTime::GetDate() const {
   return std::make_pair(month, days);
 }
 
-std::pair<int, int> DateTime::GetTime() const {
+std::pair<int, int> DateTime::GetHourMinute() const {
   int min = minutes_ % (60 * 24), hour = min / 60;
   min -= hour * 60;
   return std::make_pair(hour, min);
 }
 
 std::string DateTime::ToString() const {
-  auto [month, day] = GetDate();
-  auto [hour, min] = GetTime();
-  char buf[11];
+  auto [month, day] = GetMonthDay();
+  auto [hour, min] = GetHourMinute();
+  // char buf[12];
+  std::string buf(11, '\0');
   // str:  MM-dd hh:mm
   // pos:  0123456789
-  SetNumber(buf, month), buf[2] = '-', SetNumber(buf + 3, day);
+  // SetNumber(buf, month), buf[2] = '-', SetNumber(buf + 3, day);
+  SetNumber(buf.data(), month), buf[2] = '-', SetNumber(buf.data() + 3, day);
   buf[5] = ' ';
-  SetNumber(buf + 6, hour), buf[8] = ':', SetNumber(buf + 9, min);
-  buf[11] = '\0';
+  // SetNumber(buf + 6, hour), buf[8] = ':', SetNumber(buf + 9, min);
+  SetNumber(buf.data() + 6, hour), buf[8] = ':', SetNumber(buf.data() + 9, min);
   return buf;
 }
 
--- src/lib/datetime.h
+++ src/lib/datetime.h
@@ -3,12 +3,15 @@
 #include <string>
 
 namespace lin {
+class Time;
+class Date;
 class DateTime;
 /**
  * @brief 表示一段时间，精确到分钟。
  */
 class Duration {
  private:
+  friend Time;
   friend DateTime;
   int minutes_;
 
@@ -16,6 +19,55 @@ class Duration {
   Duration() : minutes_(0) {}
   explicit Duration(int minutes) : minutes_(minutes) {}
   int minutes() const;
+  friend auto operator<=>(const Duration &, const Duration &) = default;
+};
+/**
+ * @brief 表示一天中的时间（几时几分）。
+ */
+class Time {
+ private:
+  int minutes_;
+  friend Date;
+  friend DateTime;
+
+ public:
+  Time() : minutes_(0) {}
+  explicit Time(int minutes) : minutes_(minutes) {}
+  /**
+   * @brief 从格式为「hh:mm」的字符串解析时间。
+   */
+  explicit Time(std::string_view str);
+  std::pair<int, int> GetHourMinute() const;
+  /**
+   * @brief 转化为「hh:mm」格式的字符串。
+   */
+  std::string ToString() const;
+  Time operator+(Duration o) const;
+  Time &operator+=(Duration o);
+  Duration operator-(const Time &o) const;
+};
+/**
+ * @brief 表示一个日期（几月几日）。
+ */
+class Date {
+ private:
+  int minutes_;
+  friend DateTime;
+
+ public:
+  Date() : minutes_(0) {}
+  /**
+   * @brief 从格式为「MM-dd」的字符串解析日期。
+   */
+  explicit Date(std::string_view str);
+  std::pair<int, int> GetMonthDay() const;
+  /**
+   * @brief 转化为「MM-dd」格式的字符串。
+   */
+  std::string ToString() const;
+  DateTime operator+(Time o) const;
+  DateTime &operator+=(Time o);
+  friend auto operator<=>(const Date &, const Date &) = default;
 };
 /**
  * @brief 简陋的时间类型，精确到分钟，只能正确处理 2021 年 6 月至 8 月内的时间。
@@ -26,13 +78,17 @@ class DateTime {
 
  public:
   DateTime() : minutes_(0) {}
+  DateTime(Date date, Time time) : minutes_(date.minutes_ + time.minutes_) {}
   explicit DateTime(int minutes) : minutes_(minutes) {}
   /**
    * @brief 从格式为「MM-dd hh:mm」的字符串解析时间。
    */
-  explicit DateTime(const std::string &str);
-  std::pair<int, int> GetDate() const;
-  std::pair<int, int> GetTime() const;
+  explicit DateTime(std::string_view str);
+  std::pair<int, int> GetMonthDay() const;
+  std::pair<int, int> GetHourMinute() const;
+  /**
+   * @brief 转化为「MM-dd hh:mm」格式的字符串。
+   */
   std::string ToString() const;
   DateTime operator+(Duration o) const;
   DateTime &operator+=(Duration o);
